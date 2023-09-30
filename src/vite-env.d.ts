7bb944985b13b5fcfb481a499c2626cbea095087
a-ryang <chnwu.kim@gmail.com>
2023-09-30 21:51:43 +0900

    chore: 서버 페이징 응답 타입

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,30 @@ interface ImportMetaEnv {
 }
 
 // business
+/**
+ * @example interface ApiResponse {
+ *   items: Type[];
+ *   pageInfo: PageInfo
+ * }
+ */
+interface PageInfo {
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+/**
+ * @example interface ApiResponse extends CursorPageInfo
+ * */
+interface CursorPageInfo {
+  currentPage: number;
+  size: number;
+  itemsSize: number;
+  nextItemId: number;
+  lastPage: boolean;
+}
+
 interface Member {
   id: number;
   nickname: string;
@@ -14,3 +38,20 @@ interface Member {
 }
 
 type MemberRole = "USER" | "NEW_MEMBER" | "MEMBER" | "ADMIN";
+
+interface Article {
+  id: number;
+  title: string;
+  url: string;
+  description: string;
+  views: number;
+  hearts: number;
+  tags: Tag[];
+  ogImage: string | null;
+}
+
+interface Tag {
+  id: number;
+  name: string;
+  count?: number;
+}
