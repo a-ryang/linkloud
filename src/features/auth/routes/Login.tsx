10c4ec9ea5db299fb38a99ee8229789e5594c3c1
a-ryang <chnwu.kim@gmail.com>
2023-10-06 14:36:15 +0900

    style: 로그인 페이지 디자인

--- src/features/auth/routes/Login.tsx
+++ src/features/auth/routes/Login.tsx
@@ -1,11 +1,4 @@
-import {
-  Card,
-  Center,
-  Image,
-  Stack,
-  Text,
-  useMantineTheme,
-} from "@mantine/core";
+import { Container, Title, Image, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 import NotFound from "@/features/misc/routes/NotFound";
@@ -14,32 +7,44 @@ import ROUTES_PATH from "@/routes/routesPath";
 import GoogleButton from "../components/GoogleButton";
 import useAuth from "../hooks/useAuth";
 
+import classes from "./Login.module.css";
+
 export default function Login() {
-  const theme = useMantineTheme();
   const { isLoggedIn } = useAuth();
 
   if (isLoggedIn) return <NotFound />;
 
   return (
-    <Center mih="100vh" style={{ background: theme.colors.gray["1"] }}>
-      <Card component="section" padding="lg" radius="lg" withBorder shadow="sm">
-        <h1>
-          <Link to={ROUTES_PATH.HOME}>
-            <Image src="/logo.svg" h={36} w="auto" />
-            <span className="sr-only">링클라우드</span>
-          </Link>
-        </h1>
+    <div className={classes.wrap}>
+      <Container size="lg">
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <h1 className={classes.logo}>
+              <Link to={ROUTES_PATH.HOME}>
+                <Image src="/logo.svg" h={36} w="auto" />
+                <span className="sr-only">링클라우드</span>
+              </Link>
+            </h1>
 
-        <Text c="dimmed" my="xl" style={{ textAlign: "center" }}>
-          간편하게 시작하세요
-        </Text>
+            <Title order={2} className={classes.title}>
+              유용한{" "}
+              <Text
+                component="span"
+                inherit
+                variant="gradient"
+                gradient={{ from: "blue", to: "linkloudBlue" }}
+              >
+                링크
+              </Text>
+              를 공유하고 관리하세요
+            </Title>
 
-        <Stack>
-          <div>
-            <GoogleButton />
+            <div className={classes["social-login"]}>
+              <GoogleButton />
+            </div>
           </div>
-        </Stack>
-      </Card>
-    </Center>
+        </div>
+      </Container>
+    </div>
   );
 }
