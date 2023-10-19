5763181292f89b518a5f26b799aa446017065b9d
a-ryang <chnwu.kim@gmail.com>
2023-10-19 19:11:11 +0900

    fix menu list layout

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 
 import { AppShell } from "@/components/app-shell";
-import { Center } from "@/components/layouts";
 import { MenuList } from "@/features/menu/components";
 
 export const metadata: Metadata = {
@@ -11,9 +10,7 @@ export const metadata: Metadata = {
 export default function Menu() {
   return (
     <AppShell title="메뉴">
-      <Center>
-        <MenuList />
-      </Center>
+      <MenuList />
     </AppShell>
   );
 }
