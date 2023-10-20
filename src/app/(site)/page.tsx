efdd467e829e7eb4170bbc0d92944e50a176ca1c
a-ryang <chnwu.kim@gmail.com>
2023-10-21 00:41:40 +0900

    feat: 랜딩 팝업 #30

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,13 @@
 import { AppShell } from "@/components/app-shell";
 import { Footer } from "@/components/footer";
+import { SitePopup } from "@/components/popup";
 import { ArticleSection } from "@/features/article/components";
 import { PopularTagList } from "@/features/tag/components";
 
 export default function Home() {
   return (
     <AppShell title="홈">
+      <SitePopup />
       <PopularTagList />
       <ArticleSection />
       <Footer />
